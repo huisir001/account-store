d4638902c13789519a8dedd20e74fa2fd4029ace
huisir <273250950@qq.com>
2021-05-25 13:24:08 +0800

    -

--- sys/router/index.ts
+++ sys/router/index.ts
@@ -2,7 +2,7 @@
  * @Description: 服务分发
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-05-24 15:11:20
- * @LastEditTime: 2021-05-25 13:18:53
+ * @LastEditTime: 2021-05-25 13:23:05
  */
 
 import { saveAccount } from "../service"
@@ -17,7 +17,7 @@ export default (ipcMain: Electron.IpcMain) => {
     ipcMain.on('todo', async (event: Electron.IpcMainEvent, something: string, parames: object) => {
         let res: object
         if (Object.keys(methods).includes(something)) {
-            res = await saveAccount(parames)
+            res = await methods[something](parames)
         } else {
             res = {
                 ok: 0,

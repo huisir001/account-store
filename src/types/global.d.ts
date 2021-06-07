1cafc974d204c2579191366267de014412e606c8
huisir <273250950@qq.com>
2021-06-07 12:38:01 +0800

    -

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,8 +2,9 @@
  * @Description: 全局类型定义
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-06-07 11:29:29
- * @LastEditTime: 2021-06-07 12:27:36
+ * @LastEditTime: 2021-06-07 12:37:10
  */
 declare interface Window {
     sys: any;
+    alert: (a: string, b: string, c: string) => void;
 }

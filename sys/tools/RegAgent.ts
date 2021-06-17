3c5352ddbce72905e68ac165d1fe4e5fa895e5b1
huisir <273250950@qq.com>
2021-06-17 17:58:15 +0800

    注册表操作封装

--- sys/tools/RegAgent.ts
+++ sys/tools/RegAgent.ts
@@ -2,8 +2,113 @@
  * @Description: 注册表操作
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-06-17 15:59:18
- * @LastEditTime: 2021-06-17 16:17:21
+ * @LastEditTime: 2021-06-17 17:57:34
  */
-export default class RegAgent {
+import process from 'child_process'
 
+interface IRegArgs {
+    // 数据类型，默认REG_SZ，可为字符串或数字
+    type?: ('REG_SZ' | 'REG_MULTI_SZ' | 'REG_EXPAND_SZ' | 'REG_DWORD' | 'REG_QWORD' | 'REG_BINARY' | 'REG_NONE')
+    // 根项/根路径
+    rootDir?: ('HKCU' | 'HKCR' | 'HKLM' | 'HKU' | 'HKCC')
+    childDir: string // 子项/子路径（多级路径用反斜杠隔开，如`MyApp\UserInfo`）,项目一般用大驼峰写法
+}
+
+export default class RegAgent {
+    type: string
+    rootDir: string
+    childDir: string
+
+    constructor(childDir: string)
+    constructor(args: IRegArgs)
+    constructor(arg: IRegArgs | string) {
+        if (typeof arg == 'string') {
+            this.type = 'REG_SZ'
+            this.rootDir = 'HKCU'
+            this.childDir = arg
+        } else {
+            this.type = arg.type || 'REG_SZ'
+            this.rootDir = arg.rootDir || 'HKCU'
+            this.childDir = arg.childDir
+        }
+    }
+
+    /**
+     * @description: 子进程执行回调
+     * @param {*} error 执行出错
+     * @param {*} stdout 标准输出
+     * @param {*} stderr 标准错误
+     * @return {*}
+     */
+    static doReg(regQuery: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            process.exec(regQuery, (error: any, stdout: any, stderr: any) => {
+                if (error || stderr) {
+                    reject(error || stderr)
+                } else {
+                    resolve(stdout)
+                }
+            })
+        })
+    }
+
+    /**
+     * @description: 全路径
+     * @param {*}
+     * @return {*}
+     */
+    get regdir(): string {
+        const { rootDir, childDir } = this
+        return rootDir + '\\' + childDir
+    }
+
+    /**
+     * @description: 增
+     * @param {string} key
+     * @param {string} val
+     * @param {boolean} isReset
+     * @return {*}
+     */
+    add(key: string, val: string | number, isReset: boolean = false): any {
+        const { type, regdir } = this
+        val = typeof val == "string" ? `"${val}"` : val
+        const regQuery = `REG ADD ${regdir} /v ${key} /t ${type} /d ${val}${isReset ? ' /f' : ''}`
+
+        // 执行
+        return RegAgent.doReg(regQuery)
+    }
+
+    /**
+     * @description: 删
+     * @param {string} key
+     * @return {*}
+     */
+    del(key: string): any {
+        const regQuery = `REG DELETE ${this.regdir} /v ${key} /f`
+
+        // 执行
+        return RegAgent.doReg(regQuery)
+    }
+
+    /**
+     * @description: 改
+     * @param {string} key
+     * @param {string} val
+     * @return {*}
+     */
+    update(key: string, val: string | number): any {
+        this.add(key, val, true)
+    }
+
+    /**
+     * @description: 查
+     * @param {string} key
+     * @return {*}
+     */
+    find(key: string): any {
+        const regQuery = `REG QUERY ${this.regdir} /v ${key} /f *`
+
+        // 执行
+        return RegAgent.doReg(regQuery)
+    }
 }
